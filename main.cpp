5158f9ca5b142d5e60e342a530bfa7a7e195178a
FrzMtrsprt <alex20020703@163.com>
2023-01-12 16:54:56 +0800

    Average flops lookup (#5)
    * Minor improvements
    
    * Calculate average `fp32active` for given range
    
    * Optimize algorithm

--- main.cpp
+++ main.cpp
@@ -6,30 +6,24 @@
 #include <vector>
 #include <iostream>
 
-void loadFromFile(const std::string &path, std::string &data)
-{
-    std::ifstream ifile;
-    ifile.open(path, std::ios::in);
-    std::ostringstream ostring;
-    ostring << ifile.rdbuf();
-    data = ostring.str();
-}
+using namespace std::string_literals;
 
 int main()
 {
-    Tree tree;
-    std::string json_path = "a100-test-runner_1387429.train.1673235521507.pt.trace.json";
-    std::string data;
-    loadFromFile(json_path, data);
+    std::string path, line;
+    std::ifstream ifile;
 
-    // tree.build(data);
+    // path = "a100-test-runner_1387429.train.1673235521507.pt.trace.json"s;
+    // ifile.open(path, std::ios::in);
+    // std::getline(ifile, line, '\0');
+
+    // Tree tree;
+    // tree.build(line);
     // tree.print();
 
-    std::string csv_path = "resnet18_all_metrics.train.csv";
-    std::ifstream ifile;
-    ifile.open(csv_path, std::ios::in);
-    std::string line;
-    std::getline(ifile, line);
+    path = "resnet18_all_metrics.train.csv"s;
+    ifile.open(path, std::ios::in);
+    std::getline(ifile, line); // Ignore table header
 
     std::vector<Flops> flops_list;
     while (std::getline(ifile, line))
@@ -49,10 +43,77 @@ int main()
         flops_list.push_back(flops);
     }
 
-    for (Flops flops : flops_list)
+    uint64_t start_time;
+    for (auto i = flops_list.begin(); i < flops_list.end(); i++)
     {
-        printf("%6zu, %.2f, %5zu\n", flops.timestamp, flops.gpu_fp32active, flops.duration);
+        if (i->gpu_fp32active > 0)
+        {
+            start_time = i->timestamp - i->duration;
+            break;
+        }
     }
+
+    uint64_t end_time;
+    for (auto i = flops_list.end() - 1; i > flops_list.begin(); i--)
+    {
+        if (i->gpu_fp32active > 0)
+        {
+            end_time = i->timestamp;
+            break;
+        }
+    }
+
+    if (start_time < end_time)
+    {
+        printf("Trace starts at %zu and ends at %zu.\n", start_time, end_time);
+    }
+    else
+    {
+        printf("Invalid flops trace.\n");
+        return 0;
+    }
+
+    // for (const Flops &flops : flops_list)
+    // {
+    //     printf("%6zu, %.2f, %5zu\n", flops.timestamp, flops.gpu_fp32active, flops.duration);
+    // }
+
+    uint64_t lookup_start;
+    uint64_t lookup_end;
+    uint64_t iter_length = (end_time - start_time) / 10;
+    printf("Each iteration is %zu us long.\n", iter_length);
+    printf("Please input lookup time range: ");
+    std::cin >> lookup_start >> lookup_end;
+
+    double fp32active_sum = 0;
+    // Lookup time range projected to the first iteration
+    uint64_t iter_start = start_time + lookup_start;
+    uint64_t iter_end = start_time + lookup_end;
+    for (const Flops &flops : flops_list)
+    {
+        // Time range of this peice of flops trace
+        const uint64_t flops_start = flops.timestamp - flops.duration;
+        const uint64_t flops_end = flops.timestamp;
+        if (flops_end > iter_start && flops_start < iter_end)
+        {
+            const int time = static_cast<int>(
+                std::min(iter_end, flops_end) - std::max(iter_start, flops_start));
+            fp32active_sum += flops.gpu_fp32active * time;
+            if (flops_end >= iter_end)
+            {
+                // Move on to the next iteration
+                iter_start += iter_length;
+                iter_end += iter_length;
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+    double fp32active_avg = fp32active_sum / static_cast<int>(lookup_end - lookup_start) / 10;
+
+    printf("Average fp32active is %f", fp32active_avg);
 
     return 0;
 }
