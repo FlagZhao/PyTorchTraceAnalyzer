efd373ee7f41c457463d9c3381d58f6b7f4947cc
FrzMtrsprt <alex20020703@163.com>
2023-01-13 17:48:44 +0800

    Correlate function with kernel event

--- main.cpp
+++ main.cpp
@@ -10,26 +10,71 @@ int main()
 {
     std::string path;
 
-    // path = "a100-test-runner_1387429.train.1673235521507.pt.trace.json"s;
-    // Tree tree;
-    // tree.readFromFile(path);
-    // tree.build();
+    path = "a100-test-runner_1387429.train.1673235521507.pt.trace.json"s;
+    Tree tree;
+    tree.readFromFile(path);
+    tree.build();
     // tree.print();
 
     path = "resnet18_all_metrics.train.csv"s;
     Metrics metrics;
     metrics.readFromFile(path);
 
-    int lookup_start;
-    int lookup_end;
-    int iter_length = (metrics.end_time - metrics.start_time) / 10;
-    printf("Each iteration is %d us long.\n", iter_length);
-    printf("Please input lookup time range: ");
-    std::cin >> lookup_start >> lookup_end;
+    std::string func_name;
+    printf("Enter function name: ");
+    std::getline(std::cin, func_name);
 
-    double fp32active_avg = metrics.lookup(lookup_start, lookup_end);
+    std::vector<int> cuda_id_list;
+    int found_times = 0;
+    for (int i = 0; i < tree.event_list.size(); i++)
+    {
+        Event &event = tree.event_list[i];
+        if (tree.string_table[event.name_id] == func_name)
+        {
+            found_times++;
+            const int64_t end_time = event.timestamp + event.duration - tree.start_time;
+            for (i++;
+                 tree.event_list[i].timestamp - tree.start_time < end_time &&
+                 i < tree.event_list.size();
+                 i++)
+            {
+                event = tree.event_list[i];
+                if (event.cat == Event::cuda_runtime)
+                {
+                    cuda_id_list.push_back(i);
+                }
+            }
+            i--; // Go back to the last child function
+        }
+    }
 
-    printf("Average fp32active is %f", fp32active_avg);
+    if (found_times)
+    {
+        printf("Function found\n");
+        if (cuda_id_list.size())
+        {
+            int i = 0;
+            for (const Event &kernel : tree.kernel_list)
+            {
+                const Event &cuda = tree.event_list[cuda_id_list[i]];
+                if (cuda.correlation == kernel.correlation)
+                {
+                    printf("%s: %d\n",
+                           tree.string_table[cuda.name_id].c_str(),
+                           cuda.correlation);
+                    i++;
+                }
+            }
+        }
+        else
+        {
+            printf("This function does not invoke kernel\n");
+        }
+    }
+    else
+    {
+        printf("Function not found");
+    }
 
     return 0;
 }
