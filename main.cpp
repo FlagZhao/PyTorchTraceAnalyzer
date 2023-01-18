811ce8ea525098f30123ed8f4717ab12b3f3e826
FrzMtrsprt <alex20020703@163.com>
2023-01-18 19:55:12 +0800

    Query timer

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "query/query.h"
 #include "tree/tree.h"
 
+#include <chrono>
 #include <string>
 #include <iostream>
 
@@ -51,8 +52,14 @@ int main()
     // std::string func_name = "nn.Module: CrossEntropyLoss_0"s;
     std::string func_name = "aten::zeros|torch"s;
 
+    auto start_time = std::chrono::high_resolution_clock::now();
+
     float fp32active = query(tree, metrics, func_name, KernelUsage, KernelTime, FuzzyName);
     printf("Average fp32active: %f\n", fp32active);
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float, std::milli> duration = end_time - start_time;
+    printf("Query took %.3fms", duration.count());
 
     return 0;
 }
