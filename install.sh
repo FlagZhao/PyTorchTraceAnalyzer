ec733f35655e28a2c035fe2a21f86c2df24e695f
FindHao <find@findhao.net>
2023-01-19 10:44:16 -0500

    use cmake installed by spack

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone --depth 1 git@github.com:spack/spack.git
 source ./spack/share/spack/setup-env.sh
 spack install rapidjson
-spack load rapidjson
+spack load rapidjson cmake
 
 mkdir build && cd build
 cmake ..
