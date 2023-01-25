2f1582498e4814ec456f24868c09d66d561f6e83
FrzMtrsprt <alex20020703@163.com>
2023-01-25 16:19:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,7 @@ query.init.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
 query.init(b"./a100-test-runner_1563552.train.1673890927432.pt.trace.json",
            b"./resnet50_all_metrics.train.csv", 10)
 
-query.query_backward(b"", b"")
+query.query.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
+query.query.restype = ctypes.c_float
+avg = query.query(b"aten::zeros|torch", b"", b"", b"FuzzyName")
+print(avg)
