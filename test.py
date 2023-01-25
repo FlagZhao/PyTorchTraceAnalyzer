101b55d0bcd207f7dd8b6adff7ee8dc750fe7dfc
FrzMtrsprt <alex20020703@163.com>
2023-01-25 11:21:24 +0800

    Simplify python usage

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import ctypes
 query = ctypes.cdll.LoadLibrary("./build/libConvert.so")
 query.init.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-torch_trace = (ctypes.c_char*100)(*
-                                  bytes("./a100-test-runner_1563552.train.1673890927432.pt.trace.json", 'utf-8'))
-gpu_trace = (ctypes.c_char*100)(*bytes("./resnet50_all_metrics.train.csv", 'utf-8'))
-query.init(torch_trace, gpu_trace)
+query.init(b"./a100-test-runner_1563552.train.1673890927432.pt.trace.json",
+           b"./resnet50_all_metrics.train.csv")
+
+query.query_backward()
