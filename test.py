ac3e9953abd56505c20cf55aeaa6fbec8969f78a
FrzMtrsprt <alex20020703@163.com>
2023-01-25 14:41:00 +0800

    Update Python test case

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import ctypes
-query = ctypes.cdll.LoadLibrary("./build/libConvert.so")
-query.init.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-query.init(b"./a100-test-runner_1563552.train.1673890927432.pt.trace.json",
-           b"./resnet50_all_metrics.train.csv")
 
-query.query_backward()
+query = ctypes.cdll.LoadLibrary("./build/Release/Convert.dll")
+
+query.init.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
+query.init(b"./a100-test-runner_1563552.train.1673890927432.pt.trace.json",
+           b"./resnet50_all_metrics.train.csv", 10)
+
+query.query_backward(b"", b"")
