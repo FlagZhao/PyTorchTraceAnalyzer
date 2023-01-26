fde1356fd903c897a4eb3f6df32f51412960222f
FrzMtrsprt <alex20020703@163.com>
2023-01-26 17:56:58 +0800

    New Python class

--- test.py
+++ test.py
@@ -1,12 +1,100 @@
 import ctypes
+import sys
 
-query = ctypes.cdll.LoadLibrary("./build/Release/Convert.dll")
 
-query.init.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
-query.init(b"./a100-test-runner_1563552.train.1673890927432.pt.trace.json",
-           b"./resnet50_all_metrics.train.csv", 10)
+class Query:
+    def __init__(self, torch_trace: str = "",
+                 gpu_trace: str = "",
+                 gpu_trace_count: int = 1) -> None:
 
-query.query.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
-query.query.restype = ctypes.c_float
-avg = query.query(b"aten::zeros|torch", b"", b"", b"FuzzyName")
-print(avg)
+        if sys.platform == "win32":
+            lib_path = "./build/Release/Convert.dll"
+        else:
+            lib_path = "./build/libConvert.so"
+
+        self.cdll = ctypes.CDLL(lib_path)
+
+        if len(torch_trace) and len(gpu_trace):
+            self.cdll.init.argtypes = [ctypes.c_char_p,
+                                       ctypes.c_char_p,
+                                       ctypes.c_int]
+            self.cdll.init(torch_trace.encode("utf-8"),
+                           gpu_trace.encode("utf-8"),
+                           gpu_trace_count)
+
+    def init(self, torch_trace: str,
+             gpu_trace: str,
+             gpu_trace_count: int = 1) -> None:
+
+        self.cdll.init.argtypes = [ctypes.c_char_p,
+                                   ctypes.c_char_p,
+                                   ctypes.c_int]
+        self.cdll.init(torch_trace.encode("utf-8"),
+                       gpu_trace.encode("utf-8"),
+                       gpu_trace_count)
+
+    def query(self, func_name: str,
+              usage_query_type: str = "KernelUsage",
+              time_query_type: str = "RangeTime",
+              name_query_type: str = "PreciseName") -> float:
+
+        self.cdll.query.argtypes = [ctypes.c_char_p,
+                                    ctypes.c_char_p,
+                                    ctypes.c_char_p,
+                                    ctypes.c_char_p]
+        self.cdll.query.restype = ctypes.c_float
+
+        return self.cdll.query(func_name.encode("utf-8"),
+                               usage_query_type.encode("utf-8"),
+                               time_query_type.encode("utf-8"),
+                               name_query_type.encode("utf-8"))
+
+    def query_forward(self, usage_query_type: str = "KernelUsage",
+                      time_query_type: str = "RangeTime") -> float:
+
+        self.cdll.query_forward.argtypes = [ctypes.c_char_p,
+                                            ctypes.c_char_p]
+        self.cdll.query_forward.restype = ctypes.c_float
+
+        return self.cdll.query_forward(usage_query_type.encode("utf-8"),
+                                       time_query_type.encode("utf-8"))
+
+    def query_backward(self, usage_query_type: str = "KernelUsage",
+                       time_query_type: str = "RangeTime") -> float:
+
+        self.cdll.query_backward.argtypes = [ctypes.c_char_p,
+                                             ctypes.c_char_p]
+        self.cdll.query_backward.restype = ctypes.c_float
+
+        return self.cdll.query_backward(usage_query_type.encode("utf-8"),
+                                        time_query_type.encode("utf-8"))
+
+    def query_optimizer(self, usage_query_type: str = "KernelUsage",
+                        time_query_type: str = "RangeTime") -> float:
+
+        self.cdll.query_optimizer.argtypes = [ctypes.c_char_p,
+                                              ctypes.c_char_p]
+        self.cdll.query_optimizer.restype = ctypes.c_float
+
+        return self.cdll.query_optimizer(usage_query_type.encode("utf-8"),
+                                         time_query_type.encode("utf-8"))
+
+    def query_module(self, module_name: str,
+                     usage_query_type: str = "KernelUsage",
+                     time_query_type: str = "RangeTime") -> float:
+
+        self.cdll.query.argtypes = [ctypes.c_char_p,
+                                    ctypes.c_char_p,
+                                    ctypes.c_char_p]
+        self.cdll.query.restype = ctypes.c_float
+
+        return self.cdll.query_module(module_name.encode("utf-8"),
+                                      usage_query_type.encode("utf-8"),
+                                      time_query_type.encode("utf-8"))
+
+
+if __name__ == "__main__":
+    query = Query("./a100-test-runner_1563552.train.1673890927432.pt.trace.json",
+                  "./resnet50_all_metrics.train.csv", 10)
+    avg = query.query("aten::zeros|torch", name_query_type="FuzzyName")
+    print(avg)
